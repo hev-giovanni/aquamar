b75659808af052e1c90a90aab4d27dd5c994ab46
hev-giovanni <105686865+hev-giovanni@users.noreply.github.com>
2024-09-19 14:45:43 -0600

    usuario_encriptado

--- endpoint/usuario.php
+++ endpoint/usuario.php
@@ -91,53 +91,54 @@ INNER JOIN sucursal ON usuario.idSucursal = sucursal.idSucursal;
         break;
 
         case 'POST':
-        if ( in_array( 'Escribir', $permisos ) ) {
-            $data = json_decode( file_get_contents( 'php://input' ), true );
-
-            if (
-                !isset(
-                    $data[ 'primerNombre' ],
-                    $data[ 'segundoNombre' ],
-                    $data[ 'otrosNombres' ],
-                    $data[ 'primerApellido' ],
-                    $data[ 'segundoApellido' ],
-                    $data[ 'fechaNacimiento' ],
-                    $data[ 'telefono' ],
-                    $data[ 'nit' ],
-                    $data[ 'usuario' ],
-                    $data[ 'clave' ],
-                    $data[ 'email' ],
-                    $data[ 'idGenero' ],
-                    $data[ 'idSucursal' ],
-                    $data[ 'idStatus' ]
-                )
-            ) {
-                echo json_encode( [ 'error' => 'Datos incompletos.' ] );
-                exit();
-            }
-
-            $primerNombre = $data[ 'primerNombre' ];
-            $segundoNombre = $data[ 'segundoNombre' ];
-            $otrosNombres = $data[ 'otrosNombres' ];
-            $primerApellido = $data[ 'primerApellido' ];
-            $segundoApellido = $data[ 'segundoApellido' ];
-            $fechaNacimiento = $data[ 'fechaNacimiento' ];
-            $telefono = $data[ 'telefono' ];
-            $nit = $data[ 'nit' ];
-            $usuario = $data[ 'usuario' ];
-            $clave = $data[ 'clave' ];
-            $email = $data[ 'email' ];
-            $idGenero = $data[ 'idGenero' ];
-            $idSucursal = $data[ 'idSucursal' ];
-            $idStatus = $data[ 'idStatus' ];
-
-            $date = new DateTime( 'now', new DateTimeZone( 'UTC' ) );
-            $date->modify( '-6 hours' );
-            $fechaCreacion = $date->format( 'Y-m-d H:i:s' );
-
-            $usuarioCreacion = $userId;
-
-            $query = 'INSERT INTO usuario (
+            if (in_array('Escribir', $permisos)) {
+                $data = json_decode(file_get_contents('php://input'), true);
+        
+                if (
+                    !isset(
+                        $data['primerNombre'],
+                        $data['segundoNombre'],
+                        $data['otrosNombres'],
+                        $data['primerApellido'],
+                        $data['segundoApellido'],
+                        $data['fechaNacimiento'],
+                        $data['telefono'],
+                        $data['nit'],
+                        $data['usuario'],
+                        $data['clave'],
+                        $data['email'],
+                        $data['idGenero'],
+                        $data['idSucursal'],
+                        $data['idStatus']
+                    )
+                ) {
+                    echo json_encode(['error' => 'Datos incompletos.']);
+                    exit();
+                }
+        
+                $primerNombre = $data['primerNombre'];
+                $segundoNombre = $data['segundoNombre'];
+                $otrosNombres = $data['otrosNombres'];
+                $primerApellido = $data['primerApellido'];
+                $segundoApellido = $data['segundoApellido'];
+                $fechaNacimiento = $data['fechaNacimiento'];
+                $telefono = $data['telefono'];
+                $nit = $data['nit'];
+                $usuario = $data['usuario'];
+                // Hasheamos la clave antes de guardarla
+                $clave = password_hash($data['clave'], PASSWORD_DEFAULT);
+                $email = $data['email'];
+                $idGenero = $data['idGenero'];
+                $idSucursal = $data['idSucursal'];
+                $idStatus = $data['idStatus'];
+        
+                $date = new DateTime('now', new DateTimeZone('UTC'));
+                $date->modify('-6 hours');
+                $fechaCreacion = $date->format('Y-m-d H:i:s');
+        
+                $usuarioCreacion = $userId;
+        
+                $query = 'INSERT INTO usuario (
                     primerNombre, 
                     segundoNombre,
                     otrosNombres,
@@ -155,147 +156,145 @@ INNER JOIN sucursal ON usuario.idSucursal = sucursal.idSucursal;
                     fechaCreacion,
                     usuarioCreacion
                 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
-            if ( $insert_query = $mysqli->prepare( $query ) ) {
-                $insert_query->bind_param(
-                    'ssssssssssssssss',
-                    $primerNombre,
-                    $segundoNombre,
-                    $otrosNombres,
-                    $primerApellido,
-                    $segundoApellido,
-                    $fechaNacimiento,
-                    $telefono,
-                    $nit,
-                    $usuario,
-                    $clave,
-                    $email,
-                    $idGenero,
-                    $idSucursal,
-                    $idStatus,
-                    $fechaCreacion,
-                    $usuarioCreacion
-                );
-
-                if ( $insert_query->execute() ) {
-                    echo json_encode( [ 'success' => 'Usuario creado.' ] );
+        
+                if ($insert_query = $mysqli->prepare($query)) {
+                    $insert_query->bind_param(
+                        'ssssssssssssssss',
+                        $primerNombre,
+                        $segundoNombre,
+                        $otrosNombres,
+                        $primerApellido,
+                        $segundoApellido,
+                        $fechaNacimiento,
+                        $telefono,
+                        $nit,
+                        $usuario,
+                        $clave,
+                        $email,
+                        $idGenero,
+                        $idSucursal,
+                        $idStatus,
+                        $fechaCreacion,
+                        $usuarioCreacion
+                    );
+        
+                    if ($insert_query->execute()) {
+                        echo json_encode(['success' => 'Usuario creado.']);
+                    } else {
+                        echo json_encode(['error' => 'No se pudo crear el Usuario.']);
+                    }
+                    $insert_query->close();
                 } else {
-                    echo json_encode( [ 'error' => 'No se pudo crear el Usuario.' ] );
+                    echo json_encode(['error' => 'No se pudo preparar la consulta.']);
                 }
-                $insert_query->close();
             } else {
-                echo json_encode( [ 'error' => 'No se pudo preparar la consulta.' ] );
+                echo json_encode(['error' => 'No tienes permiso para crear datos.']);
             }
-        } else {
-            echo json_encode( [ 'error' => 'No tienes permiso para crear datos.' ] );
-        }
-        break;
-
+            break;
+        
         case 'PUT':
-        if ( in_array( 'Escribir', $permisos ) ) {
-            $data = json_decode( file_get_contents( 'php://input' ), true );
-
-            error_log( print_r( $data, true ) );
-
-            if ( !isset(
-                $data[ 'primerNombre' ],
-                $data[ 'segundoNombre' ],
-                $data[ 'otrosNombres' ],
-                $data[ 'primerApellido' ],
-                $data[ 'segundoApellido' ],
-                $data[ 'fechaNacimiento' ],
-                $data[ 'telefono' ],
-                $data[ 'nit' ],
-                $data[ 'usuario' ],
-                $data[ 'clave' ],
-                $data[ 'email' ],
-                $data[ 'idGenero' ],
-                $data[ 'idSucursal' ],
-                $data[ 'idStatus' ],
-                $data[ 'idUsuario' ]
-            ) ) {
-                echo json_encode( [ 'error' => 'Datos incompletos.' ] );
-                exit();
-            }
-
-            $primerNombre = $data[ 'primerNombre' ];
-            $segundoNombre = $data[ 'segundoNombre' ];
-            $otrosNombres = $data[ 'otrosNombres' ];
-            $primerApellido = $data[ 'primerApellido' ];
-            $segundoApellido = $data[ 'segundoApellido' ];
-            $fechaNacimiento = $data[ 'fechaNacimiento' ];
-            $telefono = $data[ 'telefono' ];
-            $nit = $data[ 'nit' ];
-            $usuario = $data[ 'usuario' ];
-            $clave = $data[ 'clave' ];
-            $email = $data[ 'email' ];
-            $idGenero = $data[ 'idGenero' ];
-            $idSucursal = $data[ 'idSucursal' ];
-            $idStatus = $data[ 'idStatus' ];
-            $idUsuario = $data[ 'idUsuario' ];
-
-            $date = new DateTime( 'now', new DateTimeZone( 'UTC' ) );
-            $date->modify( '-6 hours' );
-            $fechaModificacion = $date->format( 'Y-m-d H:i:s' );
-            $usuarioModificacion = $userId;
-
-            $query = 'UPDATE usuario SET 
-    primerNombre = ?, 
-    segundoNombre = ?,
-    otrosNombres = ?,
-    primerApellido = ?,
-    segundoApellido = ?,
-    fechaNacimiento = ?,
-    telefono = ?,
-    nit = ?,
-    usuario = ?,
-    clave = ?,
-    email = ?,
-    idGenero = ?,
-    idSucursal = ?,
-    idStatus = ?,
-    fechaModificacion = ?,
-    usuarioModificacion = ?
-    WHERE idUsuario = ?';
-
-            if ( $update_query = $mysqli->prepare( $query ) ) {
-                // Cambié 'sssssssssssisissii' a 'sssssssssssisissi' ( removí una 'i' extra )
-                $update_query->bind_param(
-                    'sssssssssssisissi', // La cadena de tipos tiene que coincidir con el número de parámetros.
-                    $primerNombre,
-                    $segundoNombre,
-                    $otrosNombres,
-                    $primerApellido,
-                    $segundoApellido,
-                    $fechaNacimiento,
-                    $telefono,
-                    $nit,
-                    $usuario,
-                    $clave,
-                    $email,
-                    $idGenero,
-                    $idSucursal,
-                    $idStatus,
-                    $fechaModificacion,
-                    $usuarioModificacion,
-                    $idUsuario
-                );
-
-                if ( $update_query->execute() ) {
-                    echo json_encode( [ 'success' => 'Usuario actualizado.' ] );
-                } else {
-                    echo json_encode( [ 'error' => 'No se pudo actualizar el Usuario.' ] );
+            if (in_array('Escribir', $permisos)) {
+                $data = json_decode(file_get_contents('php://input'), true);
+        
+                if (!isset(
+                    $data['primerNombre'],
+                    $data['segundoNombre'],
+                    $data['otrosNombres'],
+                    $data['primerApellido'],
+                    $data['segundoApellido'],
+                    $data['fechaNacimiento'],
+                    $data['telefono'],
+                    $data['nit'],
+                    $data['usuario'],
+                    $data['clave'],
+                    $data['email'],
+                    $data['idGenero'],
+                    $data['idSucursal'],
+                    $data['idStatus'],
+                    $data['idUsuario']
+                )) {
+                    echo json_encode(['error' => 'Datos incompletos.']);
+                    exit();
                 }
-
-                $update_query->close();
+        
+                $primerNombre = $data['primerNombre'];
+                $segundoNombre = $data['segundoNombre'];
+                $otrosNombres = $data['otrosNombres'];
+                $primerApellido = $data['primerApellido'];
+                $segundoApellido = $data['segundoApellido'];
+                $fechaNacimiento = $data['fechaNacimiento'];
+                $telefono = $data['telefono'];
+                $nit = $data['nit'];
+                $usuario = $data['usuario'];
+                // Hasheamos la nueva clave si se proporciona
+                $clave = password_hash($data['clave'], PASSWORD_DEFAULT);
+                $email = $data['email'];
+                $idGenero = $data['idGenero'];
+                $idSucursal = $data['idSucursal'];
+                $idStatus = $data['idStatus'];
+                $idUsuario = $data['idUsuario'];
+        
+                $date = new DateTime('now', new DateTimeZone('UTC'));
+                $date->modify('-6 hours');
+                $fechaModificacion = $date->format('Y-m-d H:i:s');
+                $usuarioModificacion = $userId;
+        
+                $query = 'UPDATE usuario SET 
+                    primerNombre = ?, 
+                    segundoNombre = ?,
+                    otrosNombres = ?,
+                    primerApellido = ?,
+                    segundoApellido = ?,
+                    fechaNacimiento = ?,
+                    telefono = ?,
+                    nit = ?,
+                    usuario = ?,
+                    clave = ?,
+                    email = ?,
+                    idGenero = ?,
+                    idSucursal = ?,
+                    idStatus = ?,
+                    fechaModificacion = ?,
+                    usuarioModificacion = ?
+                    WHERE idUsuario = ?';
+        
+                if ($update_query = $mysqli->prepare($query)) {
+                    $update_query->bind_param(
+                        'sssssssssssisissi',
+                        $primerNombre,
+                        $segundoNombre,
+                        $otrosNombres,
+                        $primerApellido,
+                        $segundoApellido,
+                        $fechaNacimiento,
+                        $telefono,
+                        $nit,
+                        $usuario,
+                        $clave,
+                        $email,
+                        $idGenero,
+                        $idSucursal,
+                        $idStatus,
+                        $fechaModificacion,
+                        $usuarioModificacion,
+                        $idUsuario
+                    );
+        
+                    if ($update_query->execute()) {
+                        echo json_encode(['success' => 'Usuario actualizado.']);
+                    } else {
+                        echo json_encode(['error' => 'No se pudo actualizar el Usuario.']);
+                    }
+        
+                    $update_query->close();
+                } else {
+                    echo json_encode(['error' => 'No se pudo preparar la consulta.']);
+                }
             } else {
-                echo json_encode( [ 'error' => 'No se pudo preparar la consulta.' ] );
+                echo json_encode(['error' => 'No tienes permiso para actualizar datos.']);
             }
-        } else {
-            echo json_encode( [ 'error' => 'No tienes permiso para actualizar datos.' ] );
-        }
-        break;
-
+            break;
+        
         case 'DELETE':
         if ( in_array( 'Borrar', $permisos ) ) {
             $id = $_GET[ 'id' ];
